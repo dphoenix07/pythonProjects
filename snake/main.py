cb4f43d8e4412f3b281ad3b6f7c585557e6b9cb3
dphoenix07 <61021456+dphoenix07@users.noreply.github.com>
2021-01-02 22:22:34 -0500

    save high score

--- snake/main.py
+++ snake/main.py
@@ -26,19 +26,22 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    # Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
 
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+    # Detect collision with wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        scoreboard.reset()
+        snake.reset()
 
+    # Detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
+            snake.reset()
 
 
 screen.exitonclick()
