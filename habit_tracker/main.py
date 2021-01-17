d6df9637795d93fb243827165d58fd6f6a9e7629
dphoenix07 <61021456+dphoenix07@users.noreply.github.com>
2021-01-16 19:25:56 -0500

    initial commit

---
+++ habit_tracker/main.py
@@ -0,0 +1,62 @@
+import requests
+from datetime import datetime
+
+USERNAME = "dphoenix07"
+TOKEN = "kdfji3923kdj9f832"
+GRAPH_ID = "graph1"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": "kdfji3923kdj9f832",
+    "username": "dphoenix07",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+#response = requests.post(url=pixela_endpoint, json=user_params)
+#print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_params = {
+    "id": GRAPH_ID,
+    "name": "Coding",
+    "unit": "hours",
+    "type": "int",
+    "color": "sora"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+#response = requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+#print(response.text)
+
+
+pixel_creation_endpoint = f"{graph_endpoint}/{GRAPH_ID}"
+
+today = datetime.now()
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("How many hours did you code today? ")
+}
+
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
+print(response.text)
+
+update_endpoint = f"{pixel_creation_endpoint}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "6"
+}
+
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixel_creation_endpoint}/{today.strftime('%Y%m%d')}"
+
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
