03db49fabd3eda9dae88343902c96126503e2146
dphoenix07 <61021456+dphoenix07@users.noreply.github.com>
2021-01-22 08:43:56 -0500

    find lowest price, send sms

--- flight-deals/flight_search.py
+++ flight-deals/flight_search.py
@@ -1,7 +1,7 @@
 import requests
-
+from flight_data import FlightData
 SEARCH_ENDPOINT = "https://tequila-api.kiwi.com"
-api_key = "*********"
+api_key = "**********"
 
 
 class FlightSearch:
@@ -14,4 +14,38 @@ class FlightSearch:
         results = response.json()["locations"]
         code = results[0]["code"]
         return code
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        headers = {"apikey": api_key}
+        query = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "GBP"
+        }
+
+        response = requests.get(url=f"{SEARCH_ENDPOINT}/v2/search",
+                                headers=headers,
+                                params=query
+                                )
+
+        data = response.json()["data"][0]
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][0]["local_departure"].split("T")[0]
+        )
+        print(f"{flight_data.destination_city}: ${flight_data.price}")
+        return flight_data
 
--- flight-deals/notification_manager.py
+++ flight-deals/notification_manager.py
@@ -1,3 +1,20 @@
+from twilio.rest import Client
+
+TWILIO_SID = "********"
+TWILIO_AUTH_TOKEN = "*******"
+TWILIO_VIRTUAL_NUMBER = "********"
+TWILIO_VERIFIED_NUMBER = "********"
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        message = self.client.message.create(
+            body=message,
+            from=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER
+        )
+
+        print(message.sid)
