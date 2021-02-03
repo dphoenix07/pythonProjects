1baa8557cac6b157e0b895cf7f75ed3a2a632077
dphoenix07 <61021456+dphoenix07@users.noreply.github.com>
2021-02-03 08:04:40 -0500

    code to scrape live site

--- 100-movies/main.py
+++ 100-movies/main.py
@@ -1,31 +1,63 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open("website.html") as file:
-    content = file.read()
+# with open("website.html") as file:
+#     content = file.read()
+#
+#
+# soup = BeautifulSoup(content, "html.parser")
+# #print(soup.prettify())
+# #print(soup.title.string)
+#
+# all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+#
+# for tag in all_anchor_tags:
+#     print(tag.getText())
+#     print(tag.get("href"))
+#
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+#
+# section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading.get("class"))
+#
+# company_url = soup.select_one(selector="p a") #string is the CSS selector
+# print(company_url)
+#
+# name = soup.select_one(selector="#name")
+# print(name)
+#
+# headings = soup.select(".heading")
+# print(headings)
 
 
-soup = BeautifulSoup(content, "html.parser")
-#print(soup.prettify())
-#print(soup.title.string)
+response = requests.get("https://news.ycombinator.com/news")
+yc_webpage = response.text
 
-all_anchor_tags = soup.find_all(name="a")
-print(all_anchor_tags)
+soup = BeautifulSoup(yc_webpage, "html.parser")
+print(soup.title)
+articles = soup.find_all(name="a", class_="storylink")
 
-for tag in all_anchor_tags:
-    print(tag.getText())
-    print(tag.get("href"))
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get("href")
+    article_links.append(link)
 
-heading = soup.find(name="h1", id="name")
-print(heading)
+article_upvote = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
 
-section_heading = soup.find(name="h3", class_="heading")
-print(section_heading.get("class"))
 
-company_url = soup.select_one(selector="p a") #string is the CSS selector
-print(company_url)
+print(article_texts)
+print(article_links)
+print(article_upvote)
 
-name = soup.select_one(selector="#name")
-print(name)
+max_upvote = max(article_upvote)
+print(max_upvote)
 
-headings = soup.select(".heading")
-print(headings)
+largest_index = article_upvote.index(max_upvote)
+
+print(article_texts[largest_index])
+print(article_links[largest_index])
